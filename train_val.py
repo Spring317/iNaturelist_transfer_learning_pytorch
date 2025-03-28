d25ef7aab8fc66f29b09573b30cd218491dfb0b3
HoangPham4171 <hoangpham4171@gmail.com>
2025-03-28 10:57:11 +0100

    Update validation step

--- train_val.py
+++ train_val.py
@@ -1,14 +1,18 @@
 import os
 import torch
+import json
 import torch.nn as nn
-from tqdm import tqdm
 from torchvision import transforms, models
 from torch.utils.data import Dataset, DataLoader
+from sklearn.metrics import classification_report, f1_score, accuracy_score, recall_score
 from PIL import Image
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
+print(torch.cuda.get_device_name(device))
 
+BATCH_SIZE = 128
+NUM_WORKERS = 16
 class CustomDataset(Dataset):
     def __init__(self, txt_file, root_dir, transform=None):
         self.image_labels=[]
@@ -37,6 +41,10 @@ class CustomDataset(Dataset):
         return image, label
 
 
+with open("./data/haute_garonne_other/dataset_species_labels.json") as file:
+    species_labels = json.load(file)
+
+
 transform_train = transforms.Compose([
     transforms.RandomResizedCrop(224, scale=(0.05, 1.0), ratio=(0.75, 1.33)),
     transforms.RandomHorizontalFlip(),
@@ -56,7 +64,7 @@ train_dataset = CustomDataset(
     transform=transform_train
 )
 
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=12, pin_memory=True)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS, pin_memory=True)
 
 
 model = models.mobilenet_v3_large(weights=models.MobileNet_V3_Large_Weights.DEFAULT)
@@ -132,3 +140,48 @@ for epoch in range(num_epochs):
         'output': {0: 'batch_size'}
     }
 )
+
+transform_val = transforms.Compose([
+    transforms.Resize(int(224 / 0.875)),      # Resize to maintain 87.5% central fraction
+    transforms.CenterCrop(224),               # Central cropping (87.5%)
+    transforms.ToTensor(),                    # Convert image to tensor [0, 1]
+    transforms.Normalize(mean=[0.5, 0.5, 0.5],# Normalize to [-1, 1]
+                         std=[0.5, 0.5, 0.5]),
+])
+
+val_dataset = CustomDataset(
+    txt_file = "./data/haute_garonne_other/val.txt",
+    root_dir=".",
+    transform=transform_val
+)
+
+val_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS, pin_memory=True)
+model.eval()
+
+val_loss, val_correct = 0.0, 0
+
+all_preds, all_labels = [], []
+
+with torch.no_grad():
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item() * images.size(0)
+        _, preds = torch.max(outputs, 1)
+        all_preds.extend(preds.cpu().numpy())
+        all_labels.extend(preds.cpu().numpy())
+
+accuracy = accuracy_score(all_labels, all_preds)
+weighted_recall = recall_score(all_labels, all_preds, average='weighted')
+f1 = f1_score(all_labels, all_preds, average='weighted')
+
+print(f"Validation accuracy: {accuracy:.4f}")
+print(f"Weighted Recall: {weighted_recall:.4f}")
+print(f"Weighted Average F1-Score: {f1:.4f}")
+print(classification_report(all_labels, all_preds, digits=4))
