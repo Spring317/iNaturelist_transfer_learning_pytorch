8f55d24b1a6b17816b64d6429099aaa17648b870
HoangPham4171 <hoangpham4171@gmail.com>
2025-04-05 23:24:46 +0200

    New training script

--- train.py
+++ train.py
@@ -3,172 +3,72 @@ import os
 import time
 
 import torch  # type: ignore
-import torch.nn as nn  # type: ignore
-from tqdm import tqdm
-from torch.utils.data import DataLoader  # type: ignore
-from torchvision import models, transforms  # type: ignore
-from dataset_builder.core import load_config
-from utility import CustomDataset, model_builder
+from utility import save_model, model_builder, get_device, build_dataloaders, train_one_epoch, validate
+from dataset_builder import run_manifest_generator
+from dataset_builder.core import load_config, validate_config
+from dataset_builder.core.exceptions import ConfigError
 
-os.system("python dataset_orchestrator.py")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
-print(torch.cuda.get_device_name(device))
+try:
+    config = load_config("./config.yaml")
+    validate_config(config)
+except ConfigError as e:
+    print(e)
+    exit()
+
+
+target_classes = config["global"]["included_classes"]
+dst_dataset_path = config["paths"]["dst_dataset"]
+dst_dataset_name = os.path.basename(dst_dataset_path)
+output_path = config["paths"]["output_dir"]
+dst_properties_path = os.path.join(output_path, f"{dst_dataset_name}_composition.json")
+train_size = config["train_val_split"]["train_size"]
+randomness = config["train_val_split"]["random_state"]
+dominant_threshold = config["train_val_split"]["dominant_threshold"]
+
+run_manifest_generator(
+    dst_dataset_path,
+    dst_dataset_path,
+    dst_properties_path,
+    train_size,
+    randomness,
+    target_classes,
+    dominant_threshold,
+)
+
+device = get_device()
 
 with open("./data/haute_garonne/dataset_species_labels.json") as file:
     species_labels = json.load(file)
 
-config = load_config("./config.yaml")
-
 BATCH_SIZE = 64
 NUM_WORKERS = 12
-NUM_EPOCHS = 30
+NUM_EPOCHS = 1
 NUM_SPECIES = len(species_labels.keys())
-DOM_THRESHOLD = config["train_val_split"]["dominant_threshold"]
+NAME = "mobilenet_v3_large"
 
-transform_train = transforms.Compose(
-    [
-        transforms.RandomResizedCrop(224, scale=(0.05, 1.0), ratio=(0.75, 1.33)),
-        transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(
-            brightness=32.0 / 255.0, saturation=0.5, contrast=0.5, hue=0.2
-        ),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    ]
-)
-
-transform_val = transforms.Compose(
-    [
-        # Resize to maintain 87.5% central fraction
-        transforms.Resize(int(224 / 0.875)),
-        transforms.CenterCrop(224),  # Central cropping (87.5%)
-        transforms.ToTensor(),  # Convert image to tensor [0, 1]
-        transforms.Normalize(
-            mean=[0.5, 0.5, 0.5],  # Normalize to [-1, 1]
-            std=[0.5, 0.5, 0.5],
-        ),
-    ]
-)
-
-print("Loading training, validation datasets")
-train_dataset = CustomDataset(
-    data_path="./data/haute_garonne/train.parquet",
-    root_dir=".",
-    transform=transform_train,
-)
-
-val_dataset = CustomDataset(
-    data_path="./data/haute_garonne/val.parquet", root_dir=".", transform=transform_val
-)
-
-print("Creating DataLoaders")
-train_loader = DataLoader(
-    train_dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-    num_workers=NUM_WORKERS,
-    pin_memory=True,
-)
-
-val_loader = DataLoader(
-    val_dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=False,
-    num_workers=NUM_WORKERS,
-    pin_memory=True,
-)
-
-model = model_builder(models, NUM_SPECIES, device)
-
+model = model_builder(NUM_SPECIES)
 if torch.cuda.device_count() > 1:
-    print(f"Using {torch.cuda.device_count()} GPUs.")
     model = torch.nn.DataParallel(model)
-
 model = model.to(device)
 
-criterion = nn.CrossEntropyLoss()
+train_loader, val_loader = build_dataloaders("./data/haute_garonne", BATCH_SIZE, NUM_WORKERS)
+
+criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, nesterov=True)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
-print("Begin training")
+best_acc = -1.0
 for epoch in range(NUM_EPOCHS):
-    # Train phase
-    model.train()
-    train_loss, train_correct = 0.0, 0
-
     start = time.perf_counter()
-    loop = tqdm(train_loader, desc=f"[Epoch {epoch + 1}/{NUM_EPOCHS}] Training")
-    for images, labels in loop:
-        images, labels = images.to(device), labels.to(device)
-
-        optimizer.zero_grad()
-        outputs = model(images)
-        # Tensor guard
-        if labels.min() < 0 or labels.max() >= outputs.shape[1]:
-            print("Invalid labels detected!")
-            print(f"Labels: {labels}")
-            print(f"Label min: {labels.min().item()} | max: {labels.max().item()}")
-            print(f"Number of classes (output.shape[1]): {outputs.shape[1]}")
-            raise ValueError("Label out of range for CrossEntropyLoss.")
-        loss = criterion(outputs, labels)
-        loss.backward()
-        
-        optimizer.step()
-
-        train_loss += loss.item() * images.size(0)
-        _, preds = torch.max(outputs, 1)
-        train_correct += torch.sum(preds == labels.data)
-        loop.set_postfix(loss=f"{loss.item():.3f}")
-
-    train_epoch_loss = train_loss / float(len(train_loader.dataset))
-    train_epoch_acc = train_correct.double() / float(len(train_loader.dataset))
-
-    # Validation phase
-    model.eval()
-    val_loss, val_correct = 0.0, 0
-
-    with torch.no_grad():
-        for images, labels in train_loader:
-            images, labels = images.to(device), labels.to(device)
-
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-
-            val_loss += loss.item() * images.size(0)
-            _, preds = torch.max(outputs, 1)
-            val_correct += torch.sum(preds == labels.data)
-
-    val_epoch_loss = val_loss / float(len(train_loader.dataset))
-    val_epoch_acc = val_correct.double() / float(len(train_loader.dataset))
-
+    train_loss, train_acc = train_one_epoch(model, train_loader, criterion, optimizer, device)
+    val_loss, val_acc = validate(model, val_loader, criterion, device)
     scheduler.step()
     end = time.perf_counter()
-
-    print(
-        f"[Epoch {epoch + 1}/{NUM_EPOCHS}] "
-        f"Train Loss: {train_epoch_loss:.4f}, Train Acc: {train_epoch_acc:.4f}, "
-        f"Val Loss: {val_epoch_loss:.4f}, Val Acc: {val_epoch_acc:.4f}"
-    )
-    print(f"[Epoch {epoch + 1}] Time: {end - start}s")
-
-    print("Saving model")
-    torch.save(model, f"./models/mobilenet_v3_large_{DOM_THRESHOLD * 100:.0f}.pth")
-    model.eval()
-    dummy_input = torch.randn(1, 3, 224, 224, device=device)
-    model_to_export = model.module if isinstance(model, torch.nn.DataParallel) else model
-    torch.onnx.export(
-        model_to_export,  # Model being run
-        dummy_input,  # Model input
-        f"./models/mobilenet_v3_large.onnx_{DOM_THRESHOLD * 100:.0f}",  # Output ONNX filename
-        export_params=True,  # Store trained parameter weights
-        opset_version=14,  # ONNX opset version
-        do_constant_folding=True,  # Perform constant folding optimization
-        input_names=["input"],  # Model input name
-        output_names=["output"],  # Model output name
-        dynamic_axes={
-            "input": {0: "batch_size"},  # Variable batch size
-            "output": {0: "batch_size"},
-        },
-    )
+    print(f"[Epoch {epoch + 1}/{NUM_EPOCHS}] Train Loss: {train_loss:.4f} Acc: {train_acc:.4f} | Val Loss: {val_loss:.4f} Acc: {val_acc:.4f} | Time: {end - start:.2f}s |", end="")
+    start = time.perf_counter()
+    if val_acc > best_acc:
+        best_acc = val_acc
+        save_model(model, f"{NAME}_{dominant_threshold * 100:.0f}", device)
+    end = time.perf_counter()
+    print(f"Save time: {end - start:.2f}s")
