8b7b1b43f75fcbe28835b1a41d2c2272aaff2ecf
HoangPham4171 <hoangpham4171@gmail.com>
2025-04-30 00:59:33 +0200

    New feature maps analysis scripts

--- feature_maps_analysis.py
+++ feature_maps_analysis.py
@@ -2,26 +2,43 @@ import pandas as pd
 from pipeline.comparing import plot_heat_map_feature_maps
 
 # Re-import files after code execution environment reset
-base_path = "./"
+base_path = "./feature_maps/"
 csv_files = [
-    "feature_map_sizes_mobilenet_v3_large_50_prune_0.csv",
-    "feature_map_sizes_mobilenet_v3_large_50_prune_30.csv",
-    "feature_map_sizes_mobilenet_v3_large_50_prune_35.csv",
-    "feature_map_sizes_mobilenet_v3_large_50_prune_40.csv",
-    "feature_map_sizes_mobilenet_v3_large_50_prune_45.csv",
-    "feature_map_sizes_mobilenet_v3_large_50_prune_50.csv",
-    # "feature_map_sizes_mobilenet_v3_large_80_prune_0.csv",
-    # "feature_map_sizes_mobilenet_v3_large_80_prune_30.csv",
-    # "feature_map_sizes_mobilenet_v3_large_80_prune_35.csv",
-    # "feature_map_sizes_mobilenet_v3_large_80_prune_40.csv",
-    # "feature_map_sizes_mobilenet_v3_large_80_prune_45.csv",
-    # "feature_map_sizes_mobilenet_v3_large_80_prune_50.csv",
-    # "feature_map_sizes_mobilenet_v3_large_90_prune_0.csv",
-    # "feature_map_sizes_mobilenet_v3_large_90_prune_30.csv",
-    # "feature_map_sizes_mobilenet_v3_large_90_prune_35.csv",
-    # "feature_map_sizes_mobilenet_v3_large_90_prune_40.csv",
-    # "feature_map_sizes_mobilenet_v3_large_90_prune_45.csv",
-    # "feature_map_sizes_mobilenet_v3_large_90_prune_50.csv",
+    "mobilenet_v3_large_50_prune_0.csv",
+    "mobilenet_v3_large_50_prune_5.csv",
+    "mobilenet_v3_large_50_prune_10.csv",
+    "mobilenet_v3_large_50_prune_15.csv",
+    "mobilenet_v3_large_50_prune_20.csv",
+    "mobilenet_v3_large_50_prune_25.csv",
+    "mobilenet_v3_large_50_prune_30.csv",
+    "mobilenet_v3_large_50_prune_35.csv",
+    "mobilenet_v3_large_50_prune_40.csv",
+    "mobilenet_v3_large_50_prune_45.csv",
+    "mobilenet_v3_large_50_prune_50.csv",
+
+    "mobilenet_v3_large_80_prune_0.csv",
+    "mobilenet_v3_large_80_prune_5.csv",
+    "mobilenet_v3_large_80_prune_10.csv",
+    "mobilenet_v3_large_80_prune_15.csv",
+    "mobilenet_v3_large_80_prune_20.csv",
+    "mobilenet_v3_large_80_prune_25.csv",
+    "mobilenet_v3_large_80_prune_30.csv",
+    "mobilenet_v3_large_80_prune_35.csv",
+    "mobilenet_v3_large_80_prune_40.csv",
+    "mobilenet_v3_large_80_prune_45.csv",
+    "mobilenet_v3_large_80_prune_50.csv",
+
+    "mobilenet_v3_large_90_prune_0.csv",
+    "mobilenet_v3_large_90_prune_5.csv",
+    "mobilenet_v3_large_90_prune_10.csv",
+    "mobilenet_v3_large_90_prune_15.csv",
+    "mobilenet_v3_large_90_prune_20.csv",
+    "mobilenet_v3_large_90_prune_25.csv",
+    "mobilenet_v3_large_90_prune_30.csv",
+    "mobilenet_v3_large_90_prune_35.csv",
+    "mobilenet_v3_large_90_prune_40.csv",
+    "mobilenet_v3_large_90_prune_45.csv",
+    "mobilenet_v3_large_90_prune_50.csv",
 ]
 
 dataframes = []
@@ -30,4 +47,6 @@ for file in csv_files:
     df["Prune"] = file.split("_")[-1].replace(".csv", "")
     dataframes.append(df)
 
-plot_heat_map_feature_maps(dataframes, "feature_maps_50.png", "Feature Map Size per Layer Across Different Prune Levels (MobileNetV3, dom=50)")
+plot_heat_map_feature_maps(dataframes[0:11], "mobilenet_v3_large_50.png", "Feature Map Size per Layer Prune Levels (MobileNetV3, dom=50)")
+plot_heat_map_feature_maps(dataframes[11:22], "mobilenet_v3_large_80.png", "Feature Map Size per Layer Prune Levels (MobileNetV3, dom=80)")
+plot_heat_map_feature_maps(dataframes[22:], "mobilenet_v3_large_90.png", "Feature Map Size per Layer Prune Levels (MobileNetV3, dom=90)")
--- feature_maps_extractor.py
+++ feature_maps_extractor.py
@@ -1,8 +1,22 @@
 from pipeline.comparing import extracting_feature_maps
 from pipeline.utility import mobile_net_v3_large_builder, get_device
+import os
 
 device = get_device(use_cpu=True)
-model = mobile_net_v3_large_builder(device, path="./models/mobilenet_v3_large_50.pth")  
+for model_file in os.listdir("/home/tom-maverick/Documents/Final Results/MobileNetV3-modified_train_prune_retrain_multiple_thresholds_global_isomorphic/"):
+    if not model_file.endswith("pth"):
+        continue
+    print(model_file)
+    model_path = f"/home/tom-maverick/Documents/Final Results/MobileNetV3-modified_train_prune_retrain_multiple_thresholds_global_isomorphic/{model_file}"
+    model_name = model_file.replace(".pth", ".csv")
+    model = mobile_net_v3_large_builder(device, path=model_path)  
 
-df_features = extracting_feature_maps(model)
-print(df_features)
+    df_features = extracting_feature_maps(model)
+
+    df_features.to_csv(f"feature_maps/{model_name}")
+# model_path = "./model/new_model/mobilenet_v3_large_90_prune_30.pth"
+# model = mobile_net_v3_large_builder(device, path=model_path)  
+
+# df_features = extracting_feature_maps(model)
+
+# df_features.to_csv(os.path.basename(model_path).replace(".pth", ".csv"))
