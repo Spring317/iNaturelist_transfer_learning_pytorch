7fbb54124c14e364d312fe58860bc5f3899cf277
Spring317 <daoxuanquyhp@gmail.com>
2025-07-11 11:23:25 +0200

    Update validation aio scripts

---
+++ val.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#OAR -q besteffort
+#OAR -l host=1/gpu=1,walltime=1:00:00
+#OAR -p kinovis
+#OAR -O OAR_%jobid%.out
+#OAR -E OAR_%jobid%.err 
+
+# display some information about attributed resources
+hostname 
+nvidia-smi 
+module load conda 
+# make use of a python torch environment
+conda activate tinyml
+# run the training script
+python val_single.py
+# run the validation script
